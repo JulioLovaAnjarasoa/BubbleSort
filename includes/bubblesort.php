b4359523efb824a5ac188538e8b2c50eda8efb24
JulioLovaAnjarasoa <julio@hairun-technology.com>
2023-02-08 14:07:39 +0300

    BUBBLESORT implemented

--- includes/bubblesort.php
+++ includes/bubblesort.php
@@ -1,1 +1,22 @@
 <?php
+
+class BubbleSort
+{
+
+    function bubbleSort(&$array)
+    {
+        $n = sizeof($array);
+        do {
+            $newn = 0;
+            for ($j = 0; $j < $n; $j++) {
+                if ($array[$j] < $array[$j + 1]) {
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $array[$j + 1] = $temp;
+                    $newn = $j;
+                }
+            }
+            $n = $newn;
+        } while ($n != 0);
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+include 'includes/bubblesort.php';
+
+$bubblesort = new BubbleSort();
+$array = array();
+
+for ($i = 0; $i < 100; $i++) {
+    $array[$i] = rand(0, 100);
+}
+$bubblesort->bubbleSort($array);
+echo "Sorted array : \n";
+echo "<br>";
+foreach ($array as $key => $value) {
+    print('Indice ' . $key . ' => ' . $value);
+    echo "<br>";
+}
